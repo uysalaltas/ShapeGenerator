2a3f0ff6e8aabb34c0796b5fee96b395270f72b0
uysal <uysalaltas@gmail.com>
2024-05-01 23:41:43 +0300

    Sandbox: Light pos modification and Teapot update

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -6,10 +6,12 @@
 class Sandbox : public Teapot::Application
 {
 public:
-	Sandbox(const Teapot::WindowProps& props = Teapot::WindowProps::WindowProps())
-		: cylender		(0.30f, glm::vec3(1.0f, 0.15f, 0.50f))
+	Sandbox(const Teapot::WindowProps& props = Teapot::WindowProps::WindowProps()) : 
+		Teapot::Application(props)
+		, cylender		(0.30f, glm::vec3(1.0f, 0.15f, 0.50f))
 		, plane			(10, 10, 1.0f, glm::vec3(0.78f, 0.95f, 1.0f))
-		, cube			(0.30f, glm::vec3(1.0f, 0.87f, 0.0f))
+		, cube			(0.30f, glm::vec3(1.0f,  0.87f, 0.0f))
+		, cubeLight		(0.10f, glm::vec3(1.0f,  1.0f , 1.0f))
 		, pyramid		(0.30f, glm::vec3(0.20f, 0.71f, 0.29f), 2.0f, 4, 1.0f, 0.0f)
 		, sphere		(0.30f, glm::vec3(0.20f, 0.25f, 1.0f), 30.0f, 30.0f)
 		, shaderDepthBasic	("src/BasicDepth.shader")
@@ -17,18 +19,20 @@ public:
 	{
 		camera = new Teapot::Camera(cameraPos, cameraCenter, cameraUp, (int*)this->GetWindow().GetWidthRef(), (int*)this->GetWindow().GetHeigthRef());
 
-		cubeModel = new Teapot::Model(cube.ShapePositions(), cube.ShapeColors(), cube.ShapeNormals(), cube.ShapeIndices(), "Cube");
-		planeModel = new Teapot::Model(plane.ShapePositions(), plane.ShapeColors(), plane.ShapeNormals(), plane.ShapeIndices(), "Plane");
-		pyramidModel = new Teapot::Model(pyramid.ShapePositions(), pyramid.ShapeColors(), pyramid.ShapeNormals(), pyramid.ShapeIndices(), "Pyramid");
-		cylinderModel = new Teapot::Model(cylender.ShapePositions(), cylender.ShapeColors(), cylender.ShapeNormals(), cylender.ShapeIndices(), "Cylender");
-		sphereModel = new Teapot::Model(sphere.ShapePositions(), sphere.ShapeColors(), sphere.ShapeNormals(), sphere.ShapeIndices(), "Sphere");
+		cubeModel     = new Teapot::Model(cube.ShapePositions()     , cube.ShapeColors()     , cube.ShapeNormals()     , cube.ShapeIndices()     , "Cube");
+		cubeLightModel= new Teapot::Model(cubeLight.ShapePositions(), cubeLight.ShapeColors(), cubeLight.ShapeNormals(), cubeLight.ShapeIndices(), "Light");
+		planeModel    = new Teapot::Model(plane.ShapePositions()    , plane.ShapeColors()    , plane.ShapeNormals()    , plane.ShapeIndices()    , "Plane");
+		pyramidModel  = new Teapot::Model(pyramid.ShapePositions()  , pyramid.ShapeColors()  , pyramid.ShapeNormals()  , pyramid.ShapeIndices()  , "Pyramid");
+		cylinderModel = new Teapot::Model(cylender.ShapePositions() , cylender.ShapeColors() , cylender.ShapeNormals() , cylender.ShapeIndices() , "Cylender");
+		sphereModel   = new Teapot::Model(sphere.ShapePositions()   , sphere.ShapeColors()   , sphere.ShapeNormals()   , sphere.ShapeIndices()   , "Sphere");
 
-		cubeModel->Translate(glm::vec3(1.0f, 0.0f, 0.0f));
-		planeModel->Translate(glm::vec3(-5.0f, -5.0f, -0.3f));
-		pyramidModel->Translate(glm::vec3(2.0f, 0.0f, 0.0f));
-		cylinderModel->Translate(glm::vec3(-1.0f, 0.0f, 0.0f));
+		cubeModel     ->Translate(glm::vec3( 1.0f,  0.0f,  0.0f ));
+		planeModel    ->Translate(glm::vec3(-5.0f, -5.0f, -0.3f ));
+		pyramidModel  ->Translate(glm::vec3( 2.0f,  0.0f,  0.0f ));
+		cylinderModel ->Translate(glm::vec3(-1.0f,  0.0f,  0.0f ));
 
 		models.push_back(cubeModel);
+		models.push_back(cubeLightModel);
 		models.push_back(planeModel);
 		models.push_back(pyramidModel);
 		models.push_back(cylinderModel);
@@ -71,20 +75,25 @@ public:
 		shadow->BindShadow();
 		RenderScene(shaderBasic);
 
+		cubeLightModel->Translate(lightPos);
+		
 		// Ui Stuff
+		//ImGui::ShowDemoWindow();
 		ImGui::Begin("Transform", NULL, 0);
 		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+		ImGui::SliderFloat3("Light Position", &lightPos[0], -10.0f, 10.0f);
 		ImGui::End();
 	}
 
 	void RenderScene(Teapot::Shader& shader)
 	{
 		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-		cubeModel->Draw(shader);
-		planeModel->Draw(shader);
-		pyramidModel->Draw(shader);
-		cylinderModel->Draw(shader);
-		sphereModel->Draw(shader);
+		cubeModel     ->Draw(shader);
+		cubeLightModel->Draw(shader);
+		planeModel    ->Draw(shader);
+		pyramidModel  ->Draw(shader);
+		cylinderModel ->Draw(shader);
+		sphereModel   ->Draw(shader);
 	}
 
 public:
@@ -95,15 +104,17 @@ private:
 	std::vector<Teapot::Model*> models;
 
 	Shapes::Cylinder cylender;
-	Shapes::Plane plane;
-	Shapes::Cube cube;
+	Shapes::Plane    plane;
+	Shapes::Cube     cube;
+	Shapes::Cube     cubeLight;
 	Shapes::Cylinder pyramid;
-	Shapes::Sphere sphere;
+	Shapes::Sphere   sphere;
 	
 	Teapot::Camera* camera;
 	Teapot::Shadow* shadow;
 
 	Teapot::Model* cubeModel;
+	Teapot::Model* cubeLightModel;
 	Teapot::Model* planeModel;
 	Teapot::Model* cylinderModel;
 	Teapot::Model* pyramidModel;
@@ -112,11 +123,10 @@ private:
 	Teapot::Shader shaderDepthBasic;
 	Teapot::Shader shaderBasic;
 
-	glm::vec3 cameraPos = glm::vec3(3.0f, 3.0f, 3.0f);
+	glm::vec3 cameraPos    = glm::vec3(3.0f, 3.0f, 3.0f);
 	glm::vec3 cameraCenter = glm::vec3(0.0f, 0.0f, 0.0f);
-	glm::vec3 cameraUp = glm::vec3(0.0f, 0.0f, 1.0f);
-
-	glm::vec3 lightPos = glm::vec3(5.0f, 1.0f, 5.0f);
+	glm::vec3 cameraUp     = glm::vec3(0.0f, 0.0f, 1.0f);
+	glm::vec3 lightPos     = glm::vec3(1.0f, 1.0f, 5.0f);
 };
 
 int main()
