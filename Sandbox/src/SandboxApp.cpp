7bd7cb30ce2dcbc8c8a7a6642707e20c623ee88e
uysal <uysalaltas@gmail.com>
2024-05-12 00:50:48 +0300

    Sandbox: Teapot ShaderManager adaptation

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -8,16 +8,17 @@ class Sandbox : public Teapot::Application
 public:
 	Sandbox(const Teapot::WindowProps& props = Teapot::WindowProps::WindowProps()) : 
 		Teapot::Application(props)
-		, cylender		(0.30f, glm::vec3(1.0f, 0.15f, 0.50f))
-		, plane			(10, 10, 1.0f, glm::vec3(0.20f, 0.15f, 0.15f))
-		, cube			(0.30f, glm::vec3(1.0f,  0.87f, 0.0f))
-		, cubeLight		(0.10f, glm::vec3(1.0f,  1.0f , 1.0f))
-		, pyramid		(0.30f, glm::vec3(0.20f, 0.71f, 0.29f), 2.0f, 4, 1.0f, 0.0f)
-		, sphere		(0.30f, glm::vec3(0.20f, 0.25f, 1.0f), 30.0f, 30.0f)
+		, cylender (0.30f, glm::vec3(1.0f, 0.15f, 0.50f))
+		, plane    (10, 10, 1.0f, glm::vec3(0.20f, 0.15f, 0.15f))
+		, cube     (0.30f, glm::vec3(1.0f,  0.87f, 0.0f))
+		, cubeLight(0.10f, glm::vec3(1.0f,  1.0f , 1.0f))
+		, pyramid  (0.30f, glm::vec3(0.20f, 0.71f, 0.29f), 2.0f, 4, 1.0f, 0.0f)
+		, sphere   (0.30f, glm::vec3(0.20f, 0.25f, 1.0f), 30.0f, 30.0f)
 	{
 		camera = new Teapot::Camera(cameraPos, cameraCenter, cameraUp, this->GetWindow().GetWidthRef(), this->GetWindow().GetHeigthRef());
+
 		cubeModel      = new Teapot::Model(cube.ShapePositions()     , cube.ShapeColors()     , cube.ShapeNormals()     , cube.ShapeIndices()     , "Cube");
-		cubeLightModel = new Teapot::Model(cubeLight.ShapePositions(), cubeLight.ShapeColors(), cubeLight.ShapeNormals(), cubeLight.ShapeIndices(), "Light");
+		//cubeLightModel = new Teapot::Model(cubeLight.ShapePositions(), cubeLight.ShapeColors(), cubeLight.ShapeNormals(), cubeLight.ShapeIndices(), "Light");
 		planeModel     = new Teapot::Model(plane.ShapePositions()    , plane.ShapeColors()    , plane.ShapeNormals()    , plane.ShapeIndices()    , "Plane");
 		pyramidModel   = new Teapot::Model(pyramid.ShapePositions()  , pyramid.ShapeColors()  , pyramid.ShapeNormals()  , pyramid.ShapeIndices()  , "Pyramid");
 		cylinderModel  = new Teapot::Model(cylender.ShapePositions() , cylender.ShapeColors() , cylender.ShapeNormals() , cylender.ShapeIndices() , "Cylender");
@@ -28,13 +29,18 @@ public:
 		pyramidModel  ->Translate(glm::vec3( 2.0f,  0.0f,  0.0f ));
 		cylinderModel ->Translate(glm::vec3(-1.0f,  0.0f,  0.0f ));
 
-		Teapot::ShaderManager::GetInstance()->SetShaderValues(*camera);
+		shaderManager->SetShaderValues(*camera);
+		shaderManager->CreateDirectionalLight(dirLight);
+		shaderManager->CreatePointLight(pointLight);
+		shaderManager->CreatePointLight(pointLight);
+		shaderManager->CreatePointLight(pointLight);
+		shaderManager->CreatePointLight(pointLight);
+		shaderManager->CreateSpotLight(spotLight);
 	}
 
 	~Sandbox()
 	{
 		delete camera;
-		//delete shadow;
 		delete cubeModel;
 		delete planeModel;
 		delete cylinderModel;
@@ -44,7 +50,6 @@ public:
 
 	void OnUpdateAwake() override
 	{
-		//shadow->RenderShadow(lightPos, models);
 	}
 
 	void OnUpdate() override
@@ -54,20 +59,18 @@ public:
 		camera->CalculatePanCamera();
 		camera->ZoomCamera();
 
-		shaderManager->RunShader();
 		RenderScene();
 
-		cubeLightModel->Translate(shaderManager->GetLightPos());
+		//cubeLightModel->Translate(shaderManager->GetLightPos());
 		
 		// Ui Stuff
 		//ImGui::ShowDemoWindow();
+		shaderManager->UIModifySpotLight();
+		shaderManager->UIModifyPointLight();
+		shaderManager->UIModifyDirectionLight();
+
 		ImGui::Begin("Transform", NULL, 0);
 		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-		ImGui::SliderFloat3("Light Position", &shaderManager->GetLightPos()[0], -10.0f, 10.0f);
-		ImGui::SliderFloat("Ambient Strength", &ambientStrength, 0.0f, 1.0f);
-		ImGui::SliderFloat("Specular Strength", &specularStrength, 0.0f, 1.0f);
-		ImGui::SliderFloat("Shininess", &shininess, 0.0f, 256.0f);
-
 		ImGui::End();
 	}
 
@@ -75,7 +78,7 @@ public:
 	{
 		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 		cubeModel     ->Draw();
-		cubeLightModel->Draw();
+		//cubeLightModel->Draw();
 		planeModel    ->Draw();
 		pyramidModel  ->Draw();
 		cylinderModel ->Draw();
@@ -92,7 +95,7 @@ private:
 	
 	Teapot::Camera* camera;
 	Teapot::Model* cubeModel;
-	Teapot::Model* cubeLightModel;
+	//Teapot::Model* cubeLightModel;
 	Teapot::Model* planeModel;
 	Teapot::Model* cylinderModel;
 	Teapot::Model* pyramidModel;
@@ -105,6 +108,10 @@ private:
 	float ambientStrength  = 0.40f;
 	float specularStrength = 0.35f;
 	float shininess	       = 50.0f;
+
+	Teapot::DirectionalLight dirLight{};
+	Teapot::PointLight pointLight{};
+	Teapot::SpotLight spotLight{};
 };
 
 int main()
